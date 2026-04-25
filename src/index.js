031762f4e2201675772936d4584f2f668b42d69c
ostyyy <ggg468432@gmail.com>
2026-04-25 19:42:13 +0300

    implement createLogger and createLimiter functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// Closure #1
+function createLogger(prefix) {
+    return function (message) {
+        console.log(`${prefix}: ${message}`);
+    };
+}
+
+const authLogger = createLogger('AUTH');
+const apiLogger = createLogger('API');
+
+authLogger('User logged in');
+apiLogger('Request failed');
+
+// Closure #2
+function createLimiter(limit) {
+    if (!Number.isFinite(limit) || limit < 0) return null;
+    let count = 0;
+
+    return function () {
+        if (count < limit) {
+            count++;
+            return 'OK';
+        }
+        return 'Error';
+    };
+}
+
+const limited = createLimiter(3);
+
+if (!limited) {
+    console.log('Oh-Oh! Invalid input!');
+} else {
+    console.log(limited());
+    console.log(limited());
+    console.log(limited());
+
+    console.log(limited());
+}
